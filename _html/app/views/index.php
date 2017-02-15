b8e4c4d0495528b2b62c20049ac6c7276dfd745d
tjabbe <martin.goutry@gmail.com>
2017-02-15 06:00:32 +0100

    feat(formule): add formules section

--- _html/app/views/index.php
+++ _html/app/views/index.php
@@ -101,16 +101,157 @@
 
     <main>
       <div class="container">
+
         <section class="intro">
           <div class="intro__catchline">Adhérer pour donner plus de poids à notre association !</div>
           <div class="intro__content">Adhérer à <span>Vaincre l'autisme</span>, c'est très simple et vous permet de <span>vous impliquer dans la vie de notre association à votre niveau</span>, en fonction de la formule que vous choisissez ci-dessous.</div>
         </section>
+
+        <form action="#">
+          <section class="choice">
+            <div class="choice__item">
+              <div class="content">
+                <div class="choice__price">
+                  <p class="price">40<span>€</span></p>
+                  <p class="monthly-price">pas de mensualisation possible</p>
+                </div>
+                <div class="choice__info">
+                  soit <span>13,6€</span> après réduction fiscale
+                  <svg class="icon-infobox">
+                    <use xlink:href="img/sprite/sprite.svg#infobox"></use>
+                  </svg>
+                </div>
+                <div class="choice__features">
+                  <p class="choice__catchline">Soutenir l'association</p>
+                  <ul class="features">
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Accès à l'espace adhérent
+                    </li>
+                  </ul>
+                  <div>
+                    <input type="radio" id="formule-1" name="formule">
+                    <label class="btn btn--choose" for="formule-1">choisir cette formule</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="choice__item">
+              <div class="content">
+                <div class="choice__price">
+                  <p class="price">60<span>€</span></p>
+                  <p class="monthly-price">ou 5€ /mois</p>
+                </div>
+                <div class="choice__info">
+                  soit <span>20,4€</span> après réduction fiscale
+                  <svg class="icon-infobox">
+                    <use xlink:href="img/sprite/sprite.svg#infobox"></use>
+                  </svg>
+                </div>
+                <div class="choice__features">
+                  <p class="choice__catchline">Bénéficier des conseils/expertises</p>
+                  <ul class="features">
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Accès à l'espace adhérent
+                    </li>
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Carte de membre
+                    </li>
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Accès aux documents privilégiés
+                    </li>
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Espace questions/réponses
+                    </li>
+                  </ul>
+                  <div>
+                    <input type="radio" id="formule-2" name="formule">
+                    <label class="btn btn--choose" for="formule-2">choisir cette formule</label>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div class="choice__item">
+              <div class="content">
+                <div class="choice__price">
+                  <p class="price">120<span>€</span></p>
+                  <p class="monthly-price">ou 10€ /mois</p>
+                </div>
+                <div class="choice__info">
+                  soit <span>40,8€</span> après réduction fiscale
+                  <svg class="icon-infobox">
+                    <use xlink:href="img/sprite/sprite.svg#infobox"></use>
+                  </svg>
+                </div>
+                <div class="choice__features">
+                  <p class="choice__catchline">S'engager avec nous</p>
+                  <ul class="features">
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Accès à l'espace adhérent
+                    </li>
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Carte de membre
+                    </li>
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Accès aux documents privilégiés
+                    </li>
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Espace questions/réponses
+                    </li>
+                    <li class="features__item">
+                      <svg class="icon-check">
+                        <use xlink:href="img/sprite/sprite.svg#check"></use>
+                      </svg>
+                      Devenez membre actif
+                    </li>
+                  </ul>
+                  <div>
+                    <input type="radio" id="formule-3" name="formule">
+                    <label class="btn btn--choose" for="formule-3">choisir cette formule</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section class="options">
+            <div class="form-block">  </div>
+          </section>
+        </form>
+
       </div>
 
     </main>
 
     <script src="js/vendors.js"></script>
-    <script src="js/app.js"></script>
+    <script src="js/main.js"></script>
 
 </body>
 </html>
