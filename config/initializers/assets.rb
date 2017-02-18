c6742c2bbeee9eace41c0b85bcda7c55fc395838
thomas <victoria.thomas-pro@hotmail.fr>
2017-02-18 20:03:45 +0100

    feat(style): scss files / js components

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,11 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-
-#Inscription CSS
-Rails.application.config.assets.precompile += %w( app.css )
-
-#Inscription JS
-Rails.application.config.assets.precompile += %w( inscription_vendors.js )
-Rails.application.config.assets.precompile += %w( main.js )
