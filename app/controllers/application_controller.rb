a3418c591f86688e9b203ed264df0d1377d87f20
Ziserman <martinziserman@gmail.com>
2017-02-28 15:04:18 +0100

    password protected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => "vaincre",
+                               :password => "lautisme"
   protect_from_forgery with: :exception
   layout 'inscription'
 end
