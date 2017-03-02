e9e509e2a94818e539acd2ecb987f29fbba7f68a
thomas <victoria.thomas-pro@hotmail.fr>
2017-03-02 15:33:20 +0100

    feat(slimpay): slimpay base implementation

---
+++ app/controllers/concerns/slimpay.rb
@@ -0,0 +1,89 @@
+module Slimpay
+
+  @@slimpay_app_id = 'vaincrelautisme01'
+  @@slimpay_app_secret = '8pgWCFFqomyeQdHhmJRALIwujEekvH8UBsaZnw0X'
+  @@slimpay_creditor_reference = 'vaincrelautisme'
+  @@slimpay_token = String.new
+  @@slimpay_server = 'https://api-sandbox.slimpay.net'
+
+  included do
+    before_action :get_token
+  end
+
+  def self.create_order
+    order = HTTParty.post(@@links["_links"]['https://api.slimpay.net/alps#create-orders']["href"],
+        headers: {
+          'Accept' => 'application/hal+json; profile="https://api.slimpay.net/alps/v1"',
+          'Content-Type' => 'application/json',
+          'Authorization' => 'Bearer ' + @@slimpay_token
+        },
+        body: {
+          started: true,
+          creditor: {
+            reference: @@slimpay_creditor_reference
+          },
+          subscriber: {
+            reference: 'user1'
+          },
+          items: [
+            {
+                type: "signMandate",
+                autoGenReference: true,
+                mandate: {
+                    signatory: {
+                        billingAddress: {
+                            street1: "27 rue des fleurs",
+                            street2: "Bat 2",
+                            city: "Paris",
+                            postalCode: "75008",
+                            country: "FR"
+                        },
+                        honorificPrefix: "Mr",
+                        familyName: "Doe",
+                        givenName: "John",
+                        email: "change.me@slimpay.com",
+                        telephone: "+33612345678"
+                    },
+                    standard: "SEPA"
+                }
+            },
+            {
+                type: "directDebit",
+                directDebit: {
+                    amount: "20",
+                    paymentReference: "mypayment",
+                    label: "This is my Direct Debit"
+                }
+            },
+          ]
+        }.to_json
+
+      )
+  end
+
+  private
+  def get_token
+    encoded_key = Base64.strict_encode64(@@slimpay_app_id+':'+@@slimpay_app_secret)
+    authorization = 'Basic '+ encoded_key
+    @@slimpay_token = HTTParty.post(@@slimpay_server+'/oauth/token',
+        headers: {
+          'Accept'        => 'application/json',
+          'Content-Type'  => 'application/x-www-form-urlencoded',
+          'Authorization' => authorization
+        },
+        body: {
+          :grant_type => 'client_credentials',
+          :scope      => 'api'
+        }.to_query
+      )['access_token']
+
+    @@links = HTTParty.get('https://api-sandbox.slimpay.net/',
+      headers: {
+        'Accept' => 'application/hal+json; profile="https://api.slimpay.net/alps/v1"',
+        'Content-Type' => 'application/json',
+        'Authorization' => 'Bearer ' + @@slimpay_token
+      }
+    )
+  end
+
+end
