3bf1e40dd65982fe0bf29913aa630b44628fe007
Ziserman <martinziserman@gmail.com>
2017-03-08 00:13:25 +0100

    fix(confirmation) redirect to root, refactor(routes) line break

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout 'inscription'
-
-  def after_sign_in_path_for(resource)
-    new_user_confirmation_path
-  end
-
 end
--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,17 +1,19 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    url = "https://api.sandbox.paypal.com/v1/payments/payment/#{resource.payment_id}/execute"
-    payment = HTTParty.post(url,
-      headers: {
-        'Content-Type' => 'application/json',
-        'Authorization' => 'Bearer ' + resource.payment_token,
-      },
-      body: {
-        :payer_id => resource.payer_id
-      })
-    puts '---------------------'
-    puts payment
-    puts '---------------------'
+    # url = "https://api.sandbox.paypal.com/v1/payments/payment/#{resource.payment_id}/execute"
+    # payment = HTTParty.post(url,
+    #   headers: {
+    #     'Content-Type' => 'application/json',
+    #     'Authorization' => 'Bearer ' + resource.payment_token,
+    #   },
+    #   body: {
+    #     :payer_id => resource.payer_id
+    #   })
+    # puts '---------------------'
+    # puts payment
+    # puts '---------------------'
+    #
+    root_path
   end
 end
