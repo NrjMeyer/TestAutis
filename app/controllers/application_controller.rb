20758be52926817922ab9ae071d6c135d8e3b16e
Thomas Victoria <thomas@MBP-de-Thomas.mshome.net>
2017-03-29 00:00:13 +0200

    fix(prod): merge error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  http_basic_authenticate_with :name => "vaincre",
-                               :password => "lautisme"
-  protect_from_forgery with: :exception
-=======
   protect_from_forgery with: :null_session
->>>>>>> origin/merge
   layout 'inscription'
 end
