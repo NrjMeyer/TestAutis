7a1d8ab36a2e0fa41322089af6680d0ca8d4dccd
Thomas Victoria <thomas@MacBook-Pro-de-Thomas.local>
2017-03-30 22:04:58 +0200

    fix(cheque): integer id

--- db/migrate/20170319171151_add_columns_to_cheque_payment.rb
+++ db/migrate/20170319171151_add_columns_to_cheque_payment.rb
@@ -2,7 +2,7 @@ class AddColumnsToChequePayment < ActiveRecord::Migration[5.0]
   def change
     add_column :cheque_payments, :amount, :integer
     add_column :cheque_payments, :validated, :boolean
-    add_column :cheque_payments, :user_id, :string
-    add_column :cheque_payments, :cache_user_id, :string
+    add_column :cheque_payments, :user_id, :integer
+    add_column :cheque_payments, :cache_user_id, :integer
   end
 end
