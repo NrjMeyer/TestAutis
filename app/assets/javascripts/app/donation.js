baca49b94b0c5fa03ca7dcb7cfda06d60be84e90
Louis Amiot <louis.amiot92@gmail.com>
2017-04-11 21:52:22 +0200

    fix(monthly): bind monthly change to donation vaue

--- app/assets/javascripts/app/donation.js
+++ app/assets/javascripts/app/donation.js
@@ -2,6 +2,7 @@ var Donation = function () {
 
   'use strict';
   var $donateRate = $('input[name="donate-rate"]');
+  var $monthly = $('input[name="monthly"]');
 
   var init = function () {
     _initEvents();
@@ -9,11 +10,20 @@ var Donation = function () {
 
   var _initEvents = function () {
     $donateRate.on('change', _bindDonateChange);
+    $monthly.on('change', _bindDonateChangeBis);
   };
 
   var _bindDonateChange = function () {
     var val = $('input[name="donate-rate"]:checked').val();
     $('input[name="monthly"][value="'+val+'"]').prop('checked',true);
+  };
+
+  var _bindDonateChangeBis = function () {
+    
+    if ($('input[name="donate-rate"]:checked').val()){
+      var val = $('input[name="monthly"]:checked').val();
+      $('input[name="donate-rate"][value="'+val+'"]').prop('checked',true);
+    }
   };
 
   init();
