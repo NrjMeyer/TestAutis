2aef9a9085924b10ab148b9888d74d714d201465
Thomas Victoria <thomas@MacBook-Pro-de-Thomas.local>
2017-05-25 16:08:15 +0200

    cb test

--- app/controllers/concerns/cb.rb
+++ app/controllers/concerns/cb.rb
@@ -11,7 +11,7 @@ module Cb
     param = "#{param} merchant_country=fr"
     param = "#{param} amount=#{amount}" 
     param = "#{param} currency_code=978"
-    param = "#{param} data="+ amount.to_s + "|" + type + "|" + id.to_s
+    param = "#{param} data=#{amount}|#{type}|#{id}"
     param = "#{param} pathfile='#{file}/cb_payment/param/pathfile'"
     param = "#{param} normal_return_url="+Settings.cb.callback_url_success
     param = "#{param} cancel_return_url="+Settings.cb.callback_url_failure
@@ -22,6 +22,11 @@ module Cb
     cmd = pathfile + " " + param
 
     stdin, stdout, stderr = Open3.popen3(cmd)
+
+    puts '----------------------'
+    puts stdout
+    puts '----------------------'
+
     result = stdout.read[4..-1]
     return result.tr('!','')
 
